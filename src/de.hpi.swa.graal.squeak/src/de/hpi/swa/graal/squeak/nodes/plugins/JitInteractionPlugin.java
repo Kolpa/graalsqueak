3147f96002268e538a25ef3e5b928dbd1d284d7e
Teresa Lasarow <teresa.lasarow@student.hpi.de>
2020-02-12 11:07:01 +0100

    return compiled method object
    We now get the callers method of the actual compiled code object and return it as array so we can inspect them in graalsqueak.

--- src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/plugins/JitInteractionPlugin.java
+++ src/de.hpi.swa.graal.squeak/src/de/hpi/swa/graal/squeak/nodes/plugins/JitInteractionPlugin.java
@@ -5,25 +5,32 @@
  */
 package de.hpi.swa.graal.squeak.nodes.plugins;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Instrument;
+
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.RootNode;
+
 import de.hpi.swa.graal.squeak.exceptions.PrimitiveExceptions.PrimitiveFailed;
 import de.hpi.swa.graal.squeak.model.CompiledMethodObject;
+import de.hpi.swa.graal.squeak.nodes.EnterCodeNode;
 import de.hpi.swa.graal.squeak.nodes.primitives.AbstractPrimitiveFactoryHolder;
 import de.hpi.swa.graal.squeak.nodes.primitives.AbstractPrimitiveNode;
-import de.hpi.swa.graal.squeak.nodes.primitives.PrimitiveInterfaces.UnaryPrimitiveWithoutFallback;
 import de.hpi.swa.graal.squeak.nodes.primitives.PrimitiveInterfaces.BinaryPrimitive;
+import de.hpi.swa.graal.squeak.nodes.primitives.PrimitiveInterfaces.UnaryPrimitiveWithoutFallback;
 import de.hpi.swa.graal.squeak.nodes.primitives.SqueakPrimitive;
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Instrument;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Map;
 
 public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
@@ -54,25 +61,38 @@ public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
         @Specialization
         protected Object getKnownCallNodes(final Object receiver, final CompiledMethodObject value) {
-            CallTarget callTarget = value.getCallTarget();
+            final CallTarget callTarget = value.getCallTarget();
 
             try {
-                Class<?> optimizedCallTargetClass = Class.forName(
-                        "org.graalvm.compiler.truffle.runtime.OptimizedCallTarget",
-                        false,
-                        callTarget.getClass().getClassLoader()
-                );
+                final Class<?> optimizedCallTargetClass = Class.forName(
+                                "org.graalvm.compiler.truffle.runtime.OptimizedCallTarget",
+                                false,
+                                callTarget.getClass().getClassLoader());
 
                 if (!optimizedCallTargetClass.isInstance(callTarget)) {
                     return method.image.env.asGuestValue(new Object[]{});
                 }
 
-                Field knownCallNodesField = optimizedCallTargetClass.getDeclaredField("knownCallNodes");
+                final Field knownCallNodesField = optimizedCallTargetClass.getDeclaredField("knownCallNodes");
 
                 knownCallNodesField.setAccessible(true);
+                @SuppressWarnings("unchecked")
+                final ArrayList<WeakReference<DirectCallNode>> knownCallNodes = (ArrayList<WeakReference<DirectCallNode>>) knownCallNodesField.get(callTarget);
 
-                return method.image.env.asGuestValue(knownCallNodesField.get(callTarget));
-            } catch (ReflectiveOperationException e) {
+                final HashSet<CompiledMethodObject> knownCallerMethods = new HashSet<>();
+                for (final WeakReference<DirectCallNode> knownCallNode : knownCallNodes) {
+                    final DirectCallNode callNode = knownCallNode.get();
+                    if (callNode == null) {
+                        continue;
+                    }
+                    final RootNode rootNode = callNode.getRootNode();
+                    if (rootNode instanceof EnterCodeNode) {
+                        knownCallerMethods.add(((EnterCodeNode) rootNode).getCodeObject().getMethod());
+                    }
+
+                }
+                return method.image.asArrayOfObjects(knownCallerMethods.toArray());
+            } catch (final ReflectiveOperationException e) {
                 throw PrimitiveFailed.GENERIC_ERROR;
             }
         }
@@ -87,11 +107,9 @@ public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
         @Specialization
         protected Object addListenerForMethodCall(final Object receiver, final CompiledMethodObject value) {
-            Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
+            final Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
 
-            AddCallTypeListenerService service = instruments
-                    .get(ReturnAndCallTypeInstrument.ID)
-                    .lookup(AddCallTypeListenerService.class);
+            final AddCallTypeListenerService service = instruments.get(ReturnAndCallTypeInstrument.ID).lookup(AddCallTypeListenerService.class);
 
             service.addListenerForMethodCall(value);
             return true;
@@ -107,11 +125,9 @@ public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
         @Specialization
         protected Object getArgumentsForMethod(final Object receiver, final CompiledMethodObject value) {
-            Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
+            final Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
 
-            AddCallTypeListenerService service = instruments
-                    .get(ReturnAndCallTypeInstrument.ID)
-                    .lookup(AddCallTypeListenerService.class);
+            final AddCallTypeListenerService service = instruments.get(ReturnAndCallTypeInstrument.ID).lookup(AddCallTypeListenerService.class);
 
             return method.image.env.asGuestValue(service.getArgumentsForMethod(value));
         }
@@ -126,11 +142,9 @@ public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
         @Specialization
         protected Object clearMethodCallListeners(final Object receiver) {
-            Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
+            final Map<String, Instrument> instruments = Context.getCurrent().getEngine().getInstruments();
 
-            AddCallTypeListenerService service = instruments
-                    .get(ReturnAndCallTypeInstrument.ID)
-                    .lookup(AddCallTypeListenerService.class);
+            final AddCallTypeListenerService service = instruments.get(ReturnAndCallTypeInstrument.ID).lookup(AddCallTypeListenerService.class);
 
             service.clearListeners();
 
@@ -147,19 +161,18 @@ public final class JitInteractionPlugin extends AbstractPrimitiveFactoryHolder {
 
         @Specialization
         protected Object getJavaArgumentsForMethod(final Object receiver, final CompiledMethodObject value) {
-            CallTarget callTarget = value.getCallTarget();
+            final CallTarget callTarget = value.getCallTarget();
 
             try {
-                Class<?> optimizedCallTargetClass = Class.forName(
-                        "org.graalvm.compiler.truffle.runtime.OptimizedCallTarget",
-                        false,
-                        callTarget.getClass().getClassLoader()
-                );
+                final Class<?> optimizedCallTargetClass = Class.forName(
+                                "org.graalvm.compiler.truffle.runtime.OptimizedCallTarget",
+                                false,
+                                callTarget.getClass().getClassLoader());
 
-                Method getProfiledArgumentTypesMethod = optimizedCallTargetClass.getDeclaredMethod("getProfiledArgumentTypes");
+                final Method getProfiledArgumentTypesMethod = optimizedCallTargetClass.getDeclaredMethod("getProfiledArgumentTypes");
                 getProfiledArgumentTypesMethod.setAccessible(true);
                 return method.image.env.asGuestValue(getProfiledArgumentTypesMethod.invoke(value.getCallTarget()));
-            } catch (ReflectiveOperationException e) {
+            } catch (final ReflectiveOperationException e) {
                 throw PrimitiveFailed.GENERIC_ERROR;
             }
         }
